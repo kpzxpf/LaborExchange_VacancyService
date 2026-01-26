926c47dee706147c4488c8de5af03841772a22fb
Nikita Volzhin <vlzh.nik@mail.ru>
2026-01-26 21:42:10 +0500

    Update Spring Boot version, add Loki logging dependencies and configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.3.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -18,6 +18,13 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+    }
+}
+
+
 dependencies {
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -28,10 +35,12 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
 
     // Feign Client
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
     // PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
+
+    implementation("com.github.loki4j:loki-logback-appender:1.5.0")
 
     // Flyway
     implementation("org.flywaydb:flyway-core")
